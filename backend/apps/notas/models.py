021ca107068386570cf770055dd3d7d33ccc6ee3
MateoLoholaberry <mateoloholaberry@gmail.com>
2022-12-07 17:28:39 -0300

    subiendo modelos de las tablas usuario y notas

--- backend/apps/notas/models.py
+++ backend/apps/notas/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from apps.usuarios.models import Usuario
+from django.utils import timezone
 
 # Create your models here.
+class Nota(models.Model):
+    
+    titulo = models.CharField(
+        max_length=30,
+        verbose_name = 'Título'
+    )
+    
+    contenido = models.CharField(
+        max_length=255,
+        verbose_name= 'Contenido'
+    )
+    
+    fecha_creacion = models.DateField(
+        verbose_name='Fecha de creación',
+        default= timezone.now
+    )
+    
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    
+    # Configuración
+    class Meta:
+        verbose_name_plural = 'Notas'
+        db_table = 'nota'
+
+    def __str__(self):
+        return self.titulo
--- backend/apps/usuarios/models.py
+++ backend/apps/usuarios/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario (models.Model):
+
+    #Atributos
+    nombre = models.CharField(
+        max_length=40,
+        verbose_name = 'Nombre'
+    )
+
+    apellido = models.CharField(
+        max_length=40,
+        verbose_name='Apellido'
+    )
+    
+    contraseña = models.CharField(
+        max_length=255,
+        verbose_name='Contraseña'
+    )
+
+
+    # Configuración
+    class Meta:
+        verbose_name_plural = 'Usuarios'
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
